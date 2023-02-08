805572655fb1201f0dfa67e95c034087e4ab3944
Victor Mafra <victorvmafra97@gmail.com>
2023-02-08 18:22:52 -0300

    versão erro CreateRoot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom/client"
 import App from "./components/App"
 
-ReactDOM.createRoot(<App/>, document.querySelector(".root"))
+ReactDOM.createRoot(document.querySelector(".root")).render(<App/>);
