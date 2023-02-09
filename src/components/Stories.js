a7634d0806c3baa44d99164ca38ec7afc87b8510
Victor Mafra <victorvmafra97@gmail.com>
2023-02-09 17:46:32 -0300

    versão com stories - props + map

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,108 +1,35 @@
 export default function Stories() {
+    const storiesInsta = [
+        { imagem: "/assets/dog.jpg", nome: "dog" },
+        { imagem: "/assets/Ronaldinho_in_2019.jpg", nome: "ronaldinho" },
+        { imagem: "/assets/Jokic.jpg", nome: "Jokic" },
+        { imagem: "/assets/Arsenal.jpg", nome: "Arsenal" },
+        { imagem: "/assets/logo-rockets.jpg", nome: "Rockets" },
+        { imagem: "/assets/stars.jpg", nome: "Stars" },
+        { imagem: "/assets/Doncic.jpg", nome: "Magic77" },
+        { imagem: "/assets/tubo.jpg", nome: "waves_rider" },
+
+    ]
     return (
         <div class="stories">
-            <div class="caixa-stories">
-                <img
-                    class="background-stories"
-                    src="/assets/stories_background.jpg"
-                />
-                <img
-                    class="imagem"
-                    src="/assets/dog.jpg"
-                />
-                <p>dog</p>
-            </div>
-
-            <div class="caixa-stories">
-                <img
-                    class="background-stories"
-                    src="/assets/stories_background.jpg"
-                />
-                <img
-                    class="imagem"
-                    src="/assets/Ronaldinho_in_2019.jpg"
-                />
-                <p>ronaldinho</p>
-            </div>
-
-            <div class="caixa-stories">
-                <img
-                    class="background-stories"
-                    src="/assets/stories_background.jpg"
-                />
-                <img
-                    class="imagem"
-                    src="/assets/Jokic.jpg"
-                />
-                <p>Jokic</p>
-            </div>
-
-            <div class="caixa-stories">
-                <img
-                    class="background-stories"
-                    src="/assets/stories_background.jpg"
-                />
-                <img
-                    class="imagem"
-                    src="/assets/Arsenal.jpg"
-                />
-                <p>Arsenal</p>
-            </div>
-
-            <div class="caixa-stories">
-                <img
-                    class="background-stories"
-                    src="/assets/stories_background.jpg"
-                />
-                <img
-                    class="imagem"
-                    src="/assets/logo-rockets.jpg"
-                />
-                <p>Rockets</p>
-            </div>
-
-            <div class="caixa-stories">
-                <img
-                    class="background-stories"
-                    src="/assets/stories_background.jpg"
-                />
-                <img
-                    class="imagem"
-                    src="/assets/stars.jpg"
-                />
-                <p>Stars__</p>
-            </div>
-
-            <div class="caixa-stories">
-                <img
-                    class="background-stories"
-                    src="/assets/stories_background.jpg"
-                />
-                <img
-                    class="imagem"
-                    src="/assets/Doncic.jpg"
-                />
-                <p>LukaMagic77</p>
-            </div>
-
-            <div class="caixa-stories">
-                <img
-                    class="background-stories"
-                    src="/assets/stories_background.jpg"
-                />
-                <img
-                    class="imagem"
-                    src="/assets/tubo.jpg"
-                />
-                <p>waves_rider</p>
-            </div>
+            {storiesInsta.map((i) => <Story nome={i.nome} imagem={i.imagem} />)}
 
             <div class="seta">
                 <ion-icon
                     name="chevron-forward-circle"
                 ></ion-icon>
             </div>
+        </div>
+    )
 
+}
+
+function Story(props) {
+    return (
+        <div class="caixa-stories">
+            <img class="background-stories" src="/assets/stories_background.jpg" />
+            <img class="imagem" src={props.imagem} />
+            <p>{props.nome}</p>
         </div>
     )
 }
